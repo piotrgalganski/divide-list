82c5490358b081c60b49a797cbbadeb8273ef930
Piotrek Galgański <piotr.galganski84@gmail.com>
2023-10-28 20:53:20 +0200

    First commit

---
+++ src/main/java/com/piotgalganski84/App.java
@@ -0,0 +1,11 @@
+package com.piotgalganski84;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		App app = new App();
+		app.start();
+	}
+
+}
